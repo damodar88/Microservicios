3635c832dd693e17983489cfcc7dd165f06440d6
damodar88 <damodar88@icloud.com>
2020-06-18 18:22:03 -0400

    se agregan entidades del Item

---
+++ springboot-item/src/main/java/com/damodar/springbootitem/models/Producto.java
@@ -0,0 +1,12 @@
+package com.damodar.springbootitem.models;
+
+import java.util.Date;
+
+public class Producto {
+
+	private Long id;
+	private String nombre;
+	private Double precio;
+	private Date createAt;
+	
+}
